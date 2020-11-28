67f59f6396daf1a7e7a33edc702ad61ac8198db2
FreshyMichael <50490309+FreshyMichael@users.noreply.github.com>
2020-11-27 20:06:20 -0500

    Update typeWriter-anywhere.php

--- typeWriter-anywhere.php
+++ typeWriter-anywhere.php
@@ -19,7 +19,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 add_action( 'wp_enqueue_scripts', 'fs_typeWriter_scripts' );
 function fs_typeWriter_scripts() {
     $dir = plugin_dir_url(__FILE__);
-    wp_enqueue_style('fs-typeWriter-scripts', $dir . 'includes/css/styles.css', array(), '1.0.0', 'all');
+    wp_enqueue_style('fs-typeWriter-scripts', $dir . 'includes/css/styles.css', array(), '1.0.1', 'all');
     wp_register_script( 'fs_typeWriter_script', $dir . 'includes/js/typeWriter-anywhere.js');	
 	wp_enqueue_script('fs_typeWriter_script');
 }
