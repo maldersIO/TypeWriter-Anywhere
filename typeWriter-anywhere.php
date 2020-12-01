a638e534d95ea945416aad74d0c88c42a09f816c
FreshyMichael <50490309+FreshyMichael@users.noreply.github.com>
2020-12-01 06:56:47 -0500

    Update typeWriter-anywhere.php

--- typeWriter-anywhere.php
+++ typeWriter-anywhere.php
@@ -24,30 +24,5 @@ function fs_typeWriter_scripts() {
 	wp_enqueue_script('fs_typeWriter_script');
 }
 
-//______________________________________________________________________________
-// All About Updates
-
-//  Begin Version Control | Auto Update Checker
-require 'plugin-update-checker/plugin-update-checker.php';
-$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
-// ***IMPORTANT*** Update this path to New Github Repository Master Branch Path
-	'https://github.com/FreshyMichael/TypeWriter-Anywhere',
-	__FILE__,
-// ***IMPORTANT*** Update this to New Repository Master Branch Path
-	'TypeWriter-Anywhere'
-);
-//Enable Releases
-$myUpdateChecker->getVcsApi()->enableReleaseAssets();
-//Optional: If you're using a private repository, specify the access token like this:
-//
-//
-//Future Update Note: Comment in these sections and add token and branch information once private git established
-//
-//
-//$myUpdateChecker->setAuthentication('your-token-here');
-//Optional: Set the branch that contains the stable release.
-//$myUpdateChecker->setBranch('stable-branch-name');
-
-//______________________________________________________________________________
 /* TypeWriter Anywhere End */
 ?>
